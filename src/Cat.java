9c5f140cbe797c9379945eaf92f862b1173612a0
and1slim <79853636633@yandex.ru>
2022-04-09 16:54:32 +0300

    initial commit

--- src/Cat.java
+++ src/Cat.java
@@ -3,32 +3,46 @@ public class Cat
 {
     private double originWeight;
     private double weight;
-
+    private static int count;
     private double minWeight;
     private double maxWeight;
-
+    private double countFeed;
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
-
+        countFeed = 0;
+        count = count + 1;
     }
-
+   public int getCount(){
+        return count;
+   }
     public void meow()
     {
         weight = weight - 1;
         System.out.println("Meow");
     }
-
+    public double getCountFeed(){
+        return countFeed;
+    }
     public void feed(Double amount)
     {
-        weight = weight + amount;
+        if (weight < minWeight || weight > maxWeight){
+            System.out.println("Кошка не доступна");
+        }
+        else
+            weight = weight + amount;
+        countFeed = amount + countFeed;
     }
 
     public void drink(Double amount)
     {
+        if (weight < minWeight || weight > maxWeight){
+            System.out.println("Кошка не доступна");
+        }
+        else
         weight = weight + amount;
     }
 
@@ -37,12 +51,17 @@ public class Cat
         return weight;
     }
 
+public void pee(){
+        weight = weight - 10;
+}
     public String getStatus()
     {
         if(weight < minWeight) {
+            count = count - 1;
             return "Dead";
         }
         else if(weight > maxWeight) {
+            count = count - 1;
             return "Exploded";
         }
         else if(weight > originWeight) {
--- src/Loader.java
+++ src/Loader.java
@@ -3,12 +3,23 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
-        Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
 
-        System.out.println(cat.getWeight());
-        System.out.println(cat1.getWeight());
-        System.out.println(cat2.getWeight());
+        Cat cat1 = new Cat();
+        cat1.feed(cat1.getWeight()/100);
+        cat1.feed(cat1.getWeight()/100);
+        cat1.feed(cat1.getWeight()/100);
+        Cat cat2 = new Cat();
+        for (int i = 0; i < 10; i++) {
+            cat1.feed(3000.0);
+
+
+        }
+
+
+
+
+        System.out.println(cat1.getCountFeed());
     }
+
+
 }
