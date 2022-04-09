71ebdc43ff1afe2989051ccbb8d93cb0db9fde72
and1slim <79853636633@yandex.ru>
2022-04-09 13:06:29 +0300

    Initial commit

---
+++ src/Loader.java
@@ -0,0 +1,14 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        Cat cat = new Cat();
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+
+        System.out.println(cat.getWeight());
+        System.out.println(cat1.getWeight());
+        System.out.println(cat2.getWeight());
+    }
+}
